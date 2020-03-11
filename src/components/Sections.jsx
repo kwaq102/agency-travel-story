e4e956e15b990f23bcb07eaddf8fca6d443a5bd3
Kamil Gajewski <kwa89@interia.pl>
2020-03-11 10:14:19 +0100

    add section components and create section about us

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import AboutUs from './AboutUs'
 
-const Sectiona = () => {
+
+const Sections = () => {
     return (
         <>
             <div>
-                <section>1</section>
+                <AboutUs />
                 <section>2</section>
                 <section>3</section>
             </div>
@@ -12,4 +14,13 @@ const Sectiona = () => {
     );
 }
 
-export default Sectiona;
+export default Sections;
+
+
+// aboutUs
+// offer
+// whyUs
+// team
+// contact
+
+// #43b1c9
